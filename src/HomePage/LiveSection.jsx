eccc225c45ccec772c22c36958a70a3ecb3f3564
atherosflick <rbaidya2002@gmail.com>
2024-11-23 21:23:38 +0530

    Play button live section

--- src/HomePage/LiveSection.jsx
+++ src/HomePage/LiveSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Hero from '../../public/images/main.png';
+import Play from '../../public/images/Play Vector.svg';
 
 
 
@@ -13,12 +14,12 @@ export default function LiveSection() {
                 <h5 className='font-semibold text-xl'> Live</h5>
             </div>
             <div className="w-full md:w-2/5 pr-3">
-             <img src={Hero} className='static md:absolute md:-translate-y-[7.9rem] lg:-translate-y-[7.9rem] xl:-translate-y-[7.9rem] md:translate-x-12 w-[20rem] lg:translate-x-12 xl:translate-x-12 aspect-auto mx-auto' alt="image" />
+             <img src={Hero} className='static md:absolute md:-translate-y-[8.3rem] lg:-translate-y-[7.9rem] xl:-translate-y-[7.7rem] md:translate-x-12 w-[20rem] lg:translate-x-12 xl:translate-x-12 aspect-auto mx-auto' alt="image" />
             </div>
             <div className="w-full md:w-3/5 px-3 flex flex-col justify-center item-center gap-5 py-10">
-                <h1 className='bg-yellow-300 px-6 py-2 text-center font-bold text-sm md:text-xl mx-auto rounded-[9rem] w-fit'>
-                    Written Training Program
-                </h1>
+                <div className='bg-yellow-300 px-6 py-2 text-center flex justify-center items-center gap-3  mx-auto rounded-[9rem]'>
+                    <div className='font-bold text-sm md:text-xl'>Written Training Program </div><img class className='w-5 md:w-3 lg:w-5' src={Play} alt=""/>
+                </div>
                 <h2 className='text-xl sm:text-2xl md:text-2xl lg:text-4xl font-bold mx-auto mt-2 text-center'>NDA | CDS | AFCAT</h2>
                 <h3 className='text-sm sm:text-sm md:text-lg xl:text-2xl font-semibold mx-auto mt-4 text-blue-900 text-center'>Batches starting from 20th December 2024.</h3>
                 <h5 className="font-thin text-xs md:text-sm lg:text-lg mx-auto text-center">To Know more about Written Training Program </h5>
