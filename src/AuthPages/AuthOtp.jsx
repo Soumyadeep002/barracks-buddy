44619d0f4d46ad1d292a37303c30f60a926b0818
sugata009 <rupsrc009@gmail.com>
2024-11-24 22:25:49 +0530

    otp

---
+++ src/AuthPages/AuthOtp.jsx
@@ -0,0 +1,103 @@
+import React, { useRef, useState } from "react";
+import Logo from '../../public/images/logo/barrackbuddy-logo-1@2x.png'
+import { Link } from "react-router-dom";
+export default function AuthOtp() {
+  const [otp, setOtp] = useState(Array(4).fill(""));
+  const inputRefs = useRef([]);
+
+  const handleKeyDown = (e) => {
+    if (
+      !/^[0-9]{1}$/.test(e.key) &&
+      e.key !== "Backspace" &&
+      e.key !== "Delete" &&
+      e.key !== "Tab" &&
+      !e.metaKey
+    ) {
+      e.preventDefault();
+    }
+
+    if (e.key === "Delete" || e.key === "Backspace") {
+      const index = inputRefs.current.indexOf(e.target);
+      if (index > 0) {
+        setOtp((prevOtp) => [
+          ...prevOtp.slice(0, index - 1),
+          "",
+          ...prevOtp.slice(index),
+        ]);
+        inputRefs.current[index - 1].focus();
+      }
+    }
+  };
+
+  const handleInput = (e) => {
+    const { target } = e;
+    const index = inputRefs.current.indexOf(target);
+    if (target.value) {
+      setOtp((prevOtp) => [
+        ...prevOtp.slice(0, index),
+        target.value,
+        ...prevOtp.slice(index + 1),
+      ]);
+      if (index < otp.length - 1) {
+        inputRefs.current[index + 1].focus();
+      }
+    }
+  };
+
+  const handleFocus = (e) => {
+    e.target.select();
+  };
+
+  const handlePaste = (e) => {
+    e.preventDefault();
+    const text = e.clipboardData.getData("text");
+    if (!new RegExp(`^[0-9]{${otp.length}}$`).test(text)) {
+      return;
+    }
+    const digits = text.split("");
+    setOtp(digits);
+  };
+
+  return (
+    <section className="bg-white flex flex-col items-center justify-center min-h-screen">
+    <Link to="/" className="flex items-center  text-2xl font-semibold">
+      <img className="w-[12rem] h-[12rem]" src={Logo} alt="logo" />
+    </Link>
+    <p className="  pb-4 italic  font-bold text-[#3b692f]">
+        *THE OTP HAS BEEN SEND TO YOUR EMAIL
+    </p>
+    <div className=" mb-2 font-semibold">
+        Veify with otp
+    </div>
+    <div className="container flex flex-col items-center">
+      <form id="otp-form" className="flex gap-2 justify-center">
+        {otp.map((digit, index) => (
+          <input
+            key={index}
+            type="text"
+            maxLength={1}
+            value={digit}
+            onChange={handleInput}
+            onKeyDown={handleKeyDown}
+            onFocus={handleFocus}
+            onPaste={handlePaste}
+            ref={(el) => (inputRefs.current[index] = el)}
+            className="shadow-xs flex w-[64px] items-center justify-center rounded-lg border-2 border-gray-300 bg-white p-2 text-center text-2xl font-medium text-gray-5 outline-none sm:text-4xl dark:border-dark-3 dark:bg-white/5"
+          />
+        ))}
+      </form>
+      <p className=" mt-2  cursor-pointer ctext-start text-sm font-semibold text-body-color">
+            Resend Code
+          </p>
+      <div className="mt-6 sm:mt-8">
+        <button className="bg-[#2f5325] rounded-full py-3 px-6 flex items-center justify-center transition-colors duration-300 hover:bg-opacity-90 mx-auto">
+          <span className="text-white font-semibold text-sm sm:text-base">
+            Send OTP
+          </span>
+        </button>
+      </div>
+    </div>
+  </section>
+  
+  );
+}
